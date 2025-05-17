ef120785c068c8e81f4beaf10c3533bd31c39a90
YisetV <vaca.yisetl@uniagustiniana.edu.co>
2025-05-17 18:06:28 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+window.onscroll = function () {
+  const btn = document.getElementById("btn-volver-arriba");
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    btn.style.display = "block";
+  } else {
+    btn.style.display = "none";
+  }
+};
+
+
+
+// BONUS: hacer que el botón suba al principio
+document.getElementById("btn-volver-arriba").addEventListener("click", function () {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
